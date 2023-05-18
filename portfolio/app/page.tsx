9d6e45a4fe8c055a9dd2a68de91cea56eb1e9067
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-05-18 18:44:31 -0400

    add welcome

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { PurpleText } from "./theme/themedComponents";
+import styled from "styled-components";
 export default function Test() {
+  const WelcomeContainer = styled.div`
+    height: calc(100vh - 80px);
+    margin-left: 70px;
+    margin-top: 450px;
+  `;
+
   return (
     <main>
-      test
-      <Link href="/about">about</Link>
+      <section>
+        <WelcomeContainer className="md:w-3/5">
+          <small>Hello! {"I’m "}Yunze (Fred)!</small>
+          <div style={{ fontSize: "42px" }}>
+            A{" "}
+            <PurpleText fontSize="42px" fontWeight={500}>
+              Software & Hardware Engineer
+            </PurpleText>{" "}
+            extraordinaire casting magic to solve problems.
+          </div>
+        </WelcomeContainer>
+        <div className="md:w-3/8">image place holder</div>
+      </section>
     </main>
   );
 }
