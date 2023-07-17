d586f1b57aea60ca2e832c53d0ba7ea5570a4ddc
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-07-18 00:38:23 +0800

    hide model list, delete vercel json

--- portfolio/app/api/OpenAiModelList/route.ts
+++ portfolio/app/api/OpenAiModelList/route.ts
@@ -1,43 +1,43 @@
 
-const { Configuration, OpenAIApi } = require("openai");
-require("dotenv").config();
+// const { Configuration, OpenAIApi } = require("openai");
+// require("dotenv").config();
 
-import { NextRequest, NextResponse } from 'next/server';
-export async function GET(Request: NextRequest) {
+// import { NextRequest, NextResponse } from 'next/server';
+// export async function GET(Request: NextRequest) {
 
-      const apiKey = process.env.OPENAI_API_KEY
-      const url = 'https://api.openai.com/v1/models'
-      console.log(apiKey)
+//       const apiKey = process.env.OPENAI_API_KEY
+//       const url = 'https://api.openai.com/v1/models'
+//       console.log(apiKey)
     
-        console.log('try to post')
-        const response = await fetch(url, {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${apiKey}`,
-          }
-        })
+//         console.log('try to post')
+//         const response = await fetch(url, {
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${apiKey}`,
+//           }
+//         })
 
-        // if(response.ok) {
-        //   const data = await response.json()
-        //   console.log('fetched data')
+//         // if(response.ok) {
+//         //   const data = await response.json()
+//         //   console.log('fetched data')
          
-        //   return new NextResponse( data , {
-        //       status: 200,
-        //     }) 
-        // }else {
-        //   const data = await response.json()
-        //   return new NextResponse( data , {
-        //     status: 500,
-        //   })  
-        // }
+//         //   return new NextResponse( data , {
+//         //       status: 200,
+//         //     }) 
+//         // }else {
+//         //   const data = await response.json()
+//         //   return new NextResponse( data , {
+//         //     status: 500,
+//         //   })  
+//         // }
 
-        const data = await response.json()
-          console.log('fetched data')
+//         const data = await response.json()
+//           console.log('fetched data')
          
-          return NextResponse.json( data , {
-              status: 200,
-            }) 
+//           return NextResponse.json( data , {
+//               status: 200,
+//             }) 
         
-  }
+//   }
 
 
