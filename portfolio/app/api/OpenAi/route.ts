e975af9baea68e1c36eb8c559777e26ebc6600e1
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-07-17 14:31:26 +0800

    Update route.ts

--- portfolio/app/api/OpenAi/route.ts
+++ portfolio/app/api/OpenAi/route.ts
@@ -4,36 +4,38 @@ require("dotenv").config();
 import { NextRequest, NextResponse } from 'next/server'
 import fetch from 'node-fetch';
 
-export async function POST(Request: NextRequest) {
+// backend api route
 
-    const apiKey = process.env.OPENAI_API_KEY
-    const url = 'https://api.openai.com/v1/chat/completions'
-  
-    const body = JSON.stringify({
-      "model": "gpt-3.5-turbo",
-      "messages": [
-          {
-              "content": "this is a test from postman, can you say hi to me ?",
-              "role":"user"
-          }
-      ]
-    })
-  
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${apiKey}`,
-        },
-        body,
-      })
-      const data = await response.json()
-      NextResponse.json(data, {status: 200})
-    } catch (error) {
-      console.error('Error occurred:', error)
-      NextResponse.json(error, {status: 500})
+export default async function handler(req, res) {
+
+  const apiKey = process.env.OPENAI_API_KEY;
+
+  const { model, messages } = req.body;
+
+  const url = "https://api.openai.com/v1/chat/completions";
+
+  const body = JSON.stringify({
+    model,
+    messages  
+  });
+
+  try {
+    const openaiRes = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body,
+    });
+
+    const data = await openaiRes.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send(error.message);
   }
+
 }
-
-
